8573acd3b5db402f287e00d3de990a1a8d76cc36
vikrant <vikrant@cse.iitb.ac.in>
2016-07-26 09:09:27 +0530

    script to clone the project submodules

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: run.sh
+# 
+#         USAGE: ./run.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Vikrant (), vikrant@cse.iitb.ac.in
+#  ORGANIZATION: 
+#       CREATED: Tuesday 26 July 2016 09:07
+#      REVISION:  ---
+#===============================================================================
+
+git submodule update --init
+
