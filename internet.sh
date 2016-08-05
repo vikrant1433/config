8e92d1e0f523dc2c73851d635afa0c198ebad45f
vikrant <vikrant@cse.iitb.ac.in>
2016-08-06 00:05:51 +0530

    forgot to add -o redirection command in curl

--- internet.sh
+++ internet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # running script hosted on http://cse.iitb.ac.in/~vikrant/i.sh
 echo -e "logging to internet.iitb.ac.in"
-curl --silent http://cse.iitb.ac.in/~vikrant/i.sh -L -o - | sh
+curl --silent -o /dev/null --data "uname=153050031&passwd=123456@" https://internet.iitb.ac.in/index.php 
 
