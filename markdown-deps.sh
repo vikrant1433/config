47efc536003dc4d76575331aaab5953461661cf3
vikrant <vikrant@cse.iitb.ac.in>
2016-08-13 03:43:49 +0530

    install vim-instant-markdown dependencies

---
+++ markdown-deps.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+npm -g install instant-markdown-d
+sudo apt-get install -y xdg-utils
+sudo apt-get install -y curl
+sudo apt-get install -y nodejs-legacy
