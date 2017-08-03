af4dad87d09eb938311b72035e4c652c96a72ede
vikrant <vikrant@cse.iitb.ac.in>
2017-08-03 16:28:43 +0530

    initial installation script

---
+++ how_to_setup_new_linux_system.sh
@@ -0,0 +1,10 @@
+# install git
+sudo apt-get install git
+# install curl
+sudo apt-get install curl
+# clone git repo
+git clone --recursive https://github.com/vikrant1433/config.git ~/
+cd ~/config
+git submodule update --init
+
+
